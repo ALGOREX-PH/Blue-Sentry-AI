00b78c39e9a75c6a50538d793d560b2869950cb9
Danielle Meer <55391597+TIGERTIDES@users.noreply.github.com>
2024-12-24 18:03:57 +0800

    Layout Fixed

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Navbar } from '../Navbar';
 import { Footer } from '../Footer';
 
 interface LayoutProps {
@@ -10,7 +9,6 @@ export function Layout({ children }: LayoutProps) {
   return (
     <div className="min-h-screen bg-deep-900">
       <div className="fixed inset-0 bg-gradient-radial from-ocean-800/20 via-deep-900/50 to-deep-900 -z-10" />
-      <Navbar />
       <main>{children}</main>
       <Footer />
     </div>

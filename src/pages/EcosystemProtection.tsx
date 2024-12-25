7f14ffd993f35e0dd29b0b701aac19e40919f3a4
Danielle Meer <55391597+TIGERTIDES@users.noreply.github.com>
2024-12-26 01:29:28 +0800

    Updated App Routes

--- src/pages/EcosystemProtection.tsx
+++ src/pages/EcosystemProtection.tsx
@@ -3,6 +3,8 @@ import { Layout } from '../components/layout/Layout';
 import { EcosystemHero } from '../components/ecosystem/EcosystemHero';
 import { EcosystemStats } from '../components/ecosystem/EcosystemStats';
 import { HolisticApproach } from '../components/ecosystem/HolisticApproach';
+import { EcosystemAIFeatures } from '../components/ecosystem/EcosystemAIFeatures';
+import { EcosystemMap } from '../components/ecosystem/EcosystemMap';
 import { TechnologyAction } from '../components/ecosystem/TechnologyAction';
 import { JoinMission } from '../components/ecosystem/JoinMission';
 
@@ -12,6 +14,8 @@ export function EcosystemProtectionPage() {
       <EcosystemHero />
       <EcosystemStats />
       <HolisticApproach />
+      <EcosystemAIFeatures />
+      <EcosystemMap />
       <TechnologyAction />
       <JoinMission />
     </Layout>

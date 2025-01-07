33bd2b4050611995bf37fb2e3a92a83b41e2fc62
unknown <debbiemeer30@gmail.com>
2025-01-07 15:58:16 +0800

    Updated

--- src/pages/PartnerWithUs.tsx
+++ src/pages/PartnerWithUs.tsx
@@ -5,13 +5,19 @@ import { WhyPartner } from '../components/partner/WhyPartner';
 import { PartnershipTypes } from '../components/partner/PartnershipTypes';
 import { PartnerTestimonials } from '../components/partner/PartnerTestimonials';
 import { PartnerCTA } from '../components/partner/PartnerCTA';
+import { PartnerBenefits } from '../components/partner/PartnerBenefits';
+import { PartnershipModels } from '../components/partner/PartnershipModels';
+import { SuccessStories } from '../components/partner/SuccessStories';
 
 export function PartnerWithUsPage() {
   return (
     <Layout>
       <PartnerHero />
+      <PartnerBenefits />
+      <PartnershipModels />
       <WhyPartner />
       <PartnershipTypes />
+      <SuccessStories />
       <PartnerTestimonials />
       <PartnerCTA />
     </Layout>
